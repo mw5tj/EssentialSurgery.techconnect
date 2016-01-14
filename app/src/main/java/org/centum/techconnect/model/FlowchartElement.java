da49fbe07ad468d93cf1832c215e57b8502e7cd9
PhaniGaddipati <phanigaddipati@gmail.com>
2016-01-14 14:40:43 -0500

    basic model class

---
+++ app/src/main/java/org/centum/techconnect/model/FlowchartElement.java
@@ -0,0 +1,84 @@
+package org.centum.techconnect.model;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Represents 1 "state" from the flowchart.
+ */
+public class FlowchartElement {
+
+    private String id;
+    private String question;
+    private String details;
+    private String attachment;
+    private String[] options;
+    private String[] nextElemIDs;
+
+    private FlowchartElement() {
+
+    }
+
+    public static FlowchartElement fromJSON(JSONObject obj) throws JSONException {
+        FlowchartElement el = new FlowchartElement();
+        el.id = obj.getString("id");
+        el.question = obj.getString("question");
+        el.details = obj.getString("detauls");
+        el.attachment = obj.getString("attachment");
+        JSONArray opts = obj.getJSONArray("options");
+        JSONArray next = obj.getJSONArray("next_questions");
+        el.options = new String[opts.length()];
+        el.nextElemIDs = new String[next.length()];
+        if (el.options.length != el.nextElemIDs.length) {
+            throw new JSONException("'options' and 'next_questions' should be the same length!");
+        }
+
+        for (int i = 0; i < opts.length(); i++) {
+            el.options[i] = opts.getString(i);
+            el.nextElemIDs[i] = next.getString(i);
+        }
+        return el;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
+
+    public int getNumOptions() {
+        return options.length;
+    }
+
+    public boolean hasAttachment() {
+        return attachment != null;
+    }
+
+}
---
+++ app/src/main/java/org/centum/techconnect/network/NetworkHelper.java
@@ -0,0 +1,11 @@
+package org.centum.techconnect.network;
+
+/**
+ * Created by Phani on 1/14/2016.
+ */
+public class NetworkHelper {
+
+    public static final String URL = "/";
+    public static final String INDEX_FILE = "index.json";
+
+}
