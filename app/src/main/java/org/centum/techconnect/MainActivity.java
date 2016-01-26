62725332c24e985a0095a736e244a7e5b5b84b02
PhaniGaddipati <phanigaddipati@gmail.com>
2016-01-26 14:29:48 -0500

    navigationdrawer handling

--- app/src/main/java/org/centum/techconnect/MainActivity.java
+++ app/src/main/java/org/centum/techconnect/MainActivity.java
@@ -2,6 +2,8 @@ package org.centum.techconnect;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -9,8 +11,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import butterknife.Bind;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int FRAGMENT_SELF_HELP = 0;
+    @Bind(R.id.nav_view)
+    NavigationView navigationView;
+    private Fragment[] fragments = new Fragment[]{};
+    private String[] fragmentTitles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        fragmentTitles = getResources().getStringArray(R.array.fragment_titles);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -25,8 +36,8 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        setFragment(FRAGMENT_SELF_HELP);
     }
 
     @Override
@@ -42,17 +53,25 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        int fragment = -1;
         if (id == R.id.nav_self_help) {
-            // Handle the camera action
-        } else if (id == R.id.nav_resources) {
-
+            fragment = FRAGMENT_SELF_HELP;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+        setFragment(fragment);
         return true;
+    }
+
+    private void setFragment(int frag) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_fragment_container, fragments[frag])
+                .addToBackStack(null)
+                .commit();
+        setTitle(fragmentTitles[frag]);
+        navigationView.getMenu().getItem(FRAGMENT_SELF_HELP).setChecked(true);
     }
 }
